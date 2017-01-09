fb10cb3f024ddcc28cc46a107794c90a74784e38
hejiafu <hejiafu@secoo.com>
2017-01-09 11:07:33 +0800

    add p3

---
+++ p3.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        maxlength=0
+    	reserve=[]
+    	start=0
+        for i in range(len(s)):
+        	if s[i] in reserve:
+        		submaxlength=i-start
+        		if submaxlength > maxlength:
+        			maxlength=submaxlength
+        		characterStart=reserve.index(s[i])
+        		reserve[0:characterStart+1]=[]
+        		reserve.append(s[i])
+        		start+=characterStart+1
+        	elif i==len(s)-1:
+        		submaxlength=i-start+1
+        		if submaxlength > maxlength:
+        			maxlength=submaxlength
+        	else:
+        		reserve.append(s[i])
+        return maxlength
