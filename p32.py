9b3db8acd629966f673363c36e65c0ace02a9816
hejiafu <hejiafu@secoo.com>
2017-01-10 18:28:56 +0800

    mark

--- p32.py
+++ p32.py
@@ -4,44 +4,34 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        self.stack=[]
-        def findmaxlength(s):
-            fl=findright(s, len(s))
-            if fl>=0:
-                if fl+1<len(s) and s[fl+1]=')':
-                    self.stack.put((fl,fl+1))
-                else:
-                    for x,y in self.stack:
-                        
-            
-        def findright(s, end):
-            start=0
-            for i in range(len(s[:end])):
-                if s[i]=='(':
-                    start=i
-            return start
-            
-        valid=0
+        def expand(s, l, r, len1):
+            if l-1>=0 and r+1<len1:
+                if s[l-1]=='(' and s[r+1]==')':
+                    l=l-1
+                    r=r+1
+                elif:
+            pass
+        stack=[]
         len1=len(s)
-        l,r=0,len1
+        [stack.append(i) for i in range(len1) if s[i]=='(']
+        l,r=None,None
+        k=len(stack)
+        maxlength=0
         while True:
-            if l>=r: break
-            
-        for i in s[len1::-1]:
-            if s[i]=='(':
-                a+=1
-            elif a==0:
+            if k==0:
+                break
+            pos=stack[k]
+            k-=1
+            if pos+1>=len1:
                 continue
-            else:
-                a-=1
-                if a==0 and (i+1>len1 or s[i+1]==')':
-                    if i-start>res:
-                        res=i-start
-                    start=i+1
+            if s[pos+1]=')':
+                if l==None: l=pos
+                if r==None: r=pos+1
+                maxlength=r-
+                continue
+            
+        print stack
         
 if __name__ == '__main__':
     s=Solution()
-    res = s.longestValidParentheses(l1,2)
-    while res:
-        print res.val
-        res=res.next
+    s.longestValidParentheses(")()())")
