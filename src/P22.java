9a7ebf3cb7f8a9af37ff8de240e78d30ef40a3d3
hehuazheng <avaforreg@163.com>
2018-08-07 15:17:06 +0800

    add

--- src/P22.java
+++ src/P22.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class P22 {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c)
+                for (String left: generateParenthesis(c))
+                    for (String right: generateParenthesis(n-1-c))
+                        ans.add("(" + left + ")" + right);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new P22().generateParenthesis(3));
+    }
 }
